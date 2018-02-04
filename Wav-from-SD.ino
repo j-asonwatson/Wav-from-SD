28acfb33a260a4b1acb777e972706f6c20de32c9
j-asonwatson <32423748+j-asonwatson@users.noreply.github.com>
2018-02-04 16:52:01 -0600

    Initial commit

---
+++ Wav-from-SD.ino
@@ -0,0 +1,35 @@
+#include <SD.h>                      // need to include the SD library
+//#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
+//#define SD_ChipSelectPin 4  //example uses hardware SS pin 4 on Nano
+#define SD_ChipSelectPin 8  //using digital pin 4 on arduino uno
+#include <TMRpcm.h>
+#include <SPI.h>
+
+TMRpcm tmrpcm;   // create an object for use in this sketch
+
+
+void setup(){
+
+  tmrpcm.speakerPin = 9; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+  tmrpcm.setVolume(5);
+
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    Serial.println("SD fail");  
+    return;   // don't do anything more if not
+  }
+  //tmrpcm.play("James.wav"); //the sound file "James.wav" will play each time the arduino powers up, or is reset
+}
+
+
+
+void loop(){  
+
+  if(Serial.available()){    
+    if(Serial.read() == 'p'){ //send the letter p over the serial monitor to start playback
+      tmrpcm.play("James.wav");
+      Serial.write("boof!");
+    }
+  }
+
+}
